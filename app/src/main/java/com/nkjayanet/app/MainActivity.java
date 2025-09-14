1a36f9967044f1f75696d78f6bb0e2277d23d6b8
khusnan21 <khusnan21@gmail.com>
2025-09-15 00:48:06 +0700

    Update MainActivity.java

--- app/src/main/java/com/nkjayanet/app/MainActivity.java
+++ app/src/main/java/com/nkjayanet/app/MainActivity.java
@@ -1,30 +1,51 @@
 package com.nkjayanet.app;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.content.Intent;
 
 public class MainActivity extends AppCompatActivity {
+
     private WebView webView;
+    private static final String LOCAL_URL = "http://127.0.0.1:8080/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // Mulai service server
         startService(new Intent(this, ServerService.class));
 
-        // Load layout WebView
         webView = new WebView(this);
         setContentView(webView);
-
-        // Konfigurasi WebView
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.getSettings().setAllowFileAccess(true);
 
-        // Akses dashboard lokal
-        webView.loadUrl("http://127.0.0.1:8080/index.php");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                Toast.makeText(MainActivity.this, "WebView error: " + error.getDescription(), Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                Uri uri = request.getUrl();
+                return !(uri.getHost() != null && uri.getHost().equals("127.0.0.1"));
+            }
+        });
+
+        webView.loadUrl(LOCAL_URL);
+    }
+
+    @Override
+    protected void onDestroy() {
+        stopService(new Intent(this, ServerService.class));
+        super.onDestroy();
     }
 }
