71fee2532a3ca35ecfcfa97f185b4a96aad68c22
khusnan21 <khusnan21@gmail.com>
2025-09-15 02:55:50 +0700

    Delete app/src/main/java/com/nkjayanet/app/SplashActivity.java

--- app/src/main/java/com/nkjayanet/app/SplashActivity.java
+++
@@ -1,8 +0,0 @@
-public class SplashActivity extends AppCompatActivity {
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        startActivity(new Intent(this, MainActivity.class));
-        finish();
-    }
-}
