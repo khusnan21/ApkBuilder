f459eabff0f9c22649983519b00188258448730d
khusnan21 <khusnan21@gmail.com>
2025-09-15 03:17:42 +0700

    Add files via upload

---
+++ app/src/main/java/com/nkjayanet/app/SplashActivity.java
@@ -0,0 +1,8 @@
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
