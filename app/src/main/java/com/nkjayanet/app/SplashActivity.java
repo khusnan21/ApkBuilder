183f28d327ae841b5c168131aa8aa6edb90d46c5
khusnan21 <khusnan21@gmail.com>
2025-09-15 03:23:07 +0700

    Update SplashActivity.java

--- app/src/main/java/com/nkjayanet/app/SplashActivity.java
+++ app/src/main/java/com/nkjayanet/app/SplashActivity.java
@@ -1,3 +1,9 @@
+package com.nkjayanet.app;
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
